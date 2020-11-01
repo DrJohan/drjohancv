e61a13ccde1f19474e3f4ec7a4780c3d4dbffc06
DrJohan <johan@ms.pmc.edu.my>
2020-11-01 12:27:37 +0800

    Adding and modifying CSS

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -2,10 +2,10 @@ library(datadrivencv)
 library(here)
 use_datadriven_cv(
   full_name = "Johan Ibrahim",
-  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc",
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "drjohan.github.io/drjohancv/",
-  source_location = "https://github.com/nstrayer/cv",
+  data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit#gid=917338460",
+  pdf_location = "https://github.com/DrJohan/drjohancv/johan_cv.pdf",
+  html_location = "drjohan.github.io/drjohancv/index.html",
+  source_location = "https://github.com/DrJohan/drjohancv",
   output_dir = here(),
   open_files = FALSE
 )
