409b9ace703ffb726721178fecef214a31187938
DrJohan <johan@ms.pmc.edu.my>
2020-11-03 00:10:22 +0800

    Latest ver

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -3,7 +3,7 @@ library(here)
 use_datadriven_cv(
   full_name = "Johan Ibrahim",
   data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit?usp=sharing",
-  pdf_location = "https://github.com/DrJohan/drjohancv/johan_cv.pdf",
+  pdf_location = "https://github.com/DrJohan/drjohancv/drjohancv.pdf",
   html_location = "drjohan.github.io/drjohancv/cv.html",
   source_location = "https://github.com/DrJohan/drjohancv",
   output_dir = here(),
