4dbedbd7bade08fca21fe4190d1aaad051b792f5
DrJohan <johan@ms.pmc.edu.my>
2020-11-02 16:16:39 +0800

    New sharing

--- setup_with_ddcv.R
+++ setup_with_ddcv.R
@@ -2,7 +2,7 @@ library(datadrivencv)
 library(here)
 use_datadriven_cv(
   full_name = "Johan Ibrahim",
-  data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit#gid=917338460",
+  data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit?usp=sharing",
   pdf_location = "https://github.com/DrJohan/drjohancv/johan_cv.pdf",
   html_location = "drjohan.github.io/drjohancv/index.html",
   source_location = "https://github.com/DrJohan/drjohancv",
